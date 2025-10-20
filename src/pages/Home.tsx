8d30c1c2abe3f4842947dc88538e9eee6096a2f5
“MITCHYUGAN” <denmichigan123@gmail.com>
2025-10-20 18:05:35 +0100

    Write Page working fine, Changed Editor to ReactQuill, and Uploads are working fine, even images

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { ArticlesSection } from "@/components/ArticleSection"
+// import { ArticlesSection } from "@/components/ArticleSection"
 import Hero from "@/components/Hero"
 import ViewArticles from "@/components/ViewArticles"
 
@@ -6,8 +6,8 @@ const Home = () => {
   return (
     <>
         <Hero />
-        <ArticlesSection />
-        {/* <ViewArticles /> */}
+        {/* <ArticlesSection /> */}
+        <ViewArticles />
     </>
   )
 }
